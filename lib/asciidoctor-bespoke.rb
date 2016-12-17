e71070e60a8096df72ad7610a66a7bf8ec70ab9d
Guillaume Grossetie <ggrossetie@gmail.com>
2016-12-17 19:57:04 +0100

    Compile to JavaScript

--- lib/asciidoctor-bespoke.rb
+++ lib/asciidoctor-bespoke.rb
@@ -1,2 +1,7 @@
-require 'asciidoctor' unless defined? Asciidoctor::Converter
-require_relative 'asciidoctor-bespoke/converter'
+if RUBY_ENGINE == 'opal'
+  require 'asciidoctor-bespoke/converter'
+  `require('asciidoctor-template.js')`
+else
+  require 'asciidoctor' unless defined? Asciidoctor::Converter
+  require_relative 'asciidoctor-bespoke/converter'
+end
--- lib/asciidoctor-bespoke/converter.rb
+++ lib/asciidoctor-bespoke/converter.rb
@@ -1,6 +1,8 @@
-require 'asciidoctor/converter/html5'
-require 'asciidoctor/converter/composite'
-require 'asciidoctor/converter/template'
+unless RUBY_ENGINE == 'opal'
+  require 'asciidoctor/converter/html5'
+  require 'asciidoctor/converter/composite'
+  require 'asciidoctor/converter/template'
+end
 
 # Asciidoctor < 1.5.5 doesn't recognize svg tag name if followed immediately by newline
 Asciidoctor::Converter::Html5Converter.tap do |klass|
@@ -11,7 +13,7 @@ end if Asciidoctor::VERSION == '1.5.3' || Asciidoctor::VERSION == '1.5.4'
 module Asciidoctor
 module Bespoke
   class Converter < ::Asciidoctor::Converter::CompositeConverter
-    ProvidedTemplatesDir = ::File.expand_path '../../../templates', __FILE__
+    ProvidedTemplatesDir = RUBY_ENGINE == 'opal' ? 'node_modules/asciidoctor-bespoke/templates' : (::File.expand_path '../../../templates', __FILE__)
     SlimPrettyOpts = { pretty: true, indent: false }.freeze
     register_for 'bespoke'
 
@@ -20,12 +22,17 @@ module Bespoke
       if (custom_template_dirs = opts[:template_dirs])
         template_dirs += custom_template_dirs.map {|d| ::File.expand_path d }
       end
-      include_dirs = template_dirs.reverse.tap {|c| c << (::File.join c.pop, 'slim') } # first dir wins
       engine_opts = (opts[:template_engine_options] || {}).dup
-      extra_slim_opts = { include_dirs: include_dirs }
-      extra_slim_opts.update SlimPrettyOpts if Set.new(%w(1 true)).include?(ENV['SLIM_PRETTY'].to_s)
-      engine_opts[:slim] = (engine_opts.key? :slim) ? (extra_slim_opts.merge engine_opts[:slim]) : extra_slim_opts
-      template_opts = opts.merge htmlsyntax: 'html', template_engine: 'slim', template_engine_options: engine_opts
+      if RUBY_ENGINE == 'opal'
+        template_engine = 'jade'
+      else
+        template_engine = 'slim'
+        include_dirs = template_dirs.reverse.tap {|c| c << (::File.join c.pop, 'slim') } # first dir wins
+        extra_slim_opts = { include_dirs: include_dirs }
+        extra_slim_opts.update SlimPrettyOpts if Set.new(%w(1 true)).include?(ENV['SLIM_PRETTY'].to_s)
+        engine_opts[:slim] = (engine_opts.key? :slim) ? (extra_slim_opts.merge engine_opts[:slim]) : extra_slim_opts
+      end
+      template_opts = opts.merge htmlsyntax: 'html', template_engine: template_engine, template_engine_options: engine_opts
       template_converter = ::Asciidoctor::Converter::TemplateConverter.new backend, template_dirs, template_opts
       html5_converter = ::Asciidoctor::Converter::Html5Converter.new backend, opts
       super backend, template_converter, html5_converter
